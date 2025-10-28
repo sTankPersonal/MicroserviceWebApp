8665310b7aa4ca13ed18e4b7d0e6868fb4343b74
stank <sean.tank131@gmail.com>
2025-10-28 10:36:50 -0400

    Select2 dynamically updates the /List /{id} to the ajax API route

--- src/Services/RecipeMicroservice/RecipeMicroservice.Presentation/wwwroot/js/select2-ajax.js
+++ src/Services/RecipeMicroservice/RecipeMicroservice.Presentation/wwwroot/js/select2-ajax.js
@@ -2,12 +2,14 @@
     $('.select2-field').each(function () {
         const $select = $(this);
         const preselectedId = $select.val();
-        const listUrl = `${$select.data('ajax-url')}/List`;
-        const singleUrl = `${$select.data('ajax-url')}/${preselectedId}`;
+        const baseUrl = $select.data('ajaxBaseUrl');
+        const listUrl = `${baseUrl}/List`;
+        const singleUrl = `${baseUrl}/${preselectedId}`;
         const idField = $select.data('ajax-id-field');
         const textField = $select.data('ajax-text-field');
         const rawPayload = $select.attr('data-ajax-payload');
         const payloadMap = rawPayload ? JSON.parse(rawPayload) : {};
+
 
         $select.select2({
             placeholder: $select.data('placeholder'),
@@ -58,7 +60,7 @@
 
         if (preselectedId && singleUrl) {
             $.ajax({
-                url: `${singleUrl}/${preselectedId}`,
+                url: singleUrl,
                 dataType: 'json'
             }).then(function (item) {
                 if (item) {
