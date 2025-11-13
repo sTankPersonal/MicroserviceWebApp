8b9dbedfca7d3a5f710dccebd516481f17e4d3e2
stank <sean.tank131@gmail.com>
2025-11-13 09:28:51 -0500

    Updated Gitignore

---
+++ src/Template/Template/Template.Presentation/Mappers/ExampleMapper.cs
@@ -0,0 +1,7 @@
+﻿namespace Template.Presentation.Mappers
+{
+    public static class ExampleMapper
+    {
+        //public static ViewModel ToViewModel(this Dto dto) => new() {};
+    }
+}
---
+++ src/Template/Template/Template.Presentation/Models/ExampleViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Template.Presentation.Models
+{
+    public class ExampleViewModel
+    {
+    }
+}
